b403b95268bdb4e3964c42de4a3d8c09d70d6d4d
marcusstenbeck <marcus.stenbeck@gmail.com>
2014-02-01 15:29:27 +0100

    Set up test program using FSM.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 define([
-    'plix/PlixApp'
+    'plix/PlixApp',
+    'plix/FsmComponent'
 ], function(
-    PlixApp
+    PlixApp,
+    FsmComponent
 ){
     var game = new PlixApp();
     game.DEBUG = true;
@@ -11,7 +13,21 @@ define([
         /**
          * Create fireworks
          */
-        
+        var s = game.createScene('main');
+        var e = s.createEntity();
+        e.transform.position.x = 100;
+        e.transform.position.y = 100;
+        e.size.x = 10;
+        e.size.y = 10;
+        e.setComponent(new FsmComponent());
+        e.components.fsm.createState('flying')
+            .onEnter(function(ent) {
+                ent.script = function() {
+                    ent.transform.position.x += 1;
+                };
+            });
+        e.components.fsm.enterState('flying');
+
 
         /**
          * Run game
