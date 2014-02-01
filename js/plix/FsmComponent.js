21d6dfe4968c71194de12fd1f2ae82506e3bf0bd
marcusstenbeck <marcus.stenbeck@gmail.com>
2014-02-01 15:31:31 +0100

    Implement FsmComponent

--- js/plix/FsmComponent.js
+++ js/plix/FsmComponent.js
@@ -10,74 +10,24 @@ define([
         if(!params) params = {};
         this.type = 'fsm';
 
-        // The registered states
-        this.fsm = params.fsm || new FSM();
+        // The state machine
+        this._fsm = params.fsm || new FSM({owner: this.entity});
     }
-
     FsmComponent.prototype = Object.create(Component.prototype);
 
-    FsmComponent.prototype.addState = function(stateName, behavior) {
-        console.log('Add state: ', stateName);
-
-        // Return if the state already exists
-        if(this._states[stateName]) return;
-
-        // Set empty function if behavior function is missing
-        if(!behavior) behavior = function(evt) { };
-
-        // Create state
-        this._states[stateName] = behavior;
+    FsmComponent.prototype.createState = function(stateName) {
+        return this._fsm.createState(stateName);
     };
 
-    FsmComponent.prototype.deleteState = function(stateName) {
-        console.log('Delete state: ', stateName);
-
-        // Return if the state doesn't exist
-        if(!this._states[stateName]) return;
-
-        // Delete state
-        delete this._states[stateName];
+    FsmComponent.prototype.enterState = function(stateName) {
+        // TODO: If currently in a state, then run it's onExit
+        this._fsm.enterState(stateName);
+        return this;
     };
 
-    FsmComponent.prototype.getStates = function() {
-        return this._states;
-    };
-
-    FsmComponent.prototype.getState = function() {
-        return this._state;
-    };
-
-    FsmComponent.prototype.setState = function(stateName) {
-        if(!this._states[stateName]) {
-            console.warn("There's no state called", stateName);
-            return;
-        }
-
-        console.log('Changin state from', this._state , 'to', stateName);
-        this._state = stateName;
-    };    
-
-    FsmComponent.prototype.process = function() {
-        if(!this._state) {
-            console.warn('Initial state not set.');
-            return;
-        }
-
-        var _len = this._messageQueue.length;
-
-        if(_len > 0) {
-            for(var i = 0; i < _len; i++) {
-                this._states[this._state](this.entity, this._messageQueue[i]);
-            }
-
-            this._messageQueue = [];
-        } else {
-            this._states[this._state](this.entity);
-        }
-    };
-
-    FsmComponent.prototype.receiveMessage = function(message) {
-        this._messageQueue.push(message);
+    FsmComponent.prototype.setEntity = function(entity) {
+        this.entity = entity;
+        this._fsm.owner = this.entity;
     };
 
     return FsmComponent;
