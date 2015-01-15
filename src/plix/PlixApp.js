ffb523885511129c0f9cfe0c39810fce64e941a2
marcusstenbeck <marcus.stenbeck@gmail.com>
2015-01-15 06:17:30 +0100

    Move scene update and draw code to Scene class. The PlixApp class should only be responsible for telling the scene to update, but not handle the actual updating code.

--- src/plix/PlixApp.js
+++ src/plix/PlixApp.js
@@ -75,61 +75,22 @@ define([
 	};
 
     PlixApp.prototype.update = function(time) {
+    	// Update time
 		this.tpf = time - this.timeElapsed;
         this.timeElapsed = time;
-
-
-        window.requestAnimationFrame( this.update.bind(this) );
 
         // Wipe the canvas clean
         this.ctx.fillStyle = 'rgba(0,0,0,1)';
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        
-        if(this.state == 'running') {
 
-        	this.scenes.forEach(function(scene) {
-				if(scene._physicsWorld) scene._physicsWorld.update(scene.app.tpf);
-				
-				scene.entities.forEach(function(entity) {
-					if(typeof entity.script === 'function') entity.script(ent);
-					if(entity._components.physics) {
-						entity.transform.position.x = entity._components.physics.body.pos.x;
-						entity.transform.position.y = entity._components.physics.body.pos.y;
-					}
-				});
-			});
+        // For each scene in the stack
+        this.scenes.forEach(function(scene) {
+        	scene.update(scene.app.tpf);
+        	scene.draw(scene.app.ctx);
+        });
 
-			if(this.DEBUG) {
-				var ent = null;
-
-				for(var i = 0; i < this.scenes.length; i++) {
-					this.ctx.strokeStyle = this.scenes[i].active ? 'magenta' : 'grey';
-
-					for(var j = 0; j < this.scenes[i].entities.length; j++) {
-						ent = this.scenes[i].entities[j];
-
-						// Get color from entity
-						var color = ent.component('graphics').graphic.color;
-
-						this.ctx.strokeStyle = 'rgba(' + (255 * color[0]) + ',' + (255 * color[1]) + ',' + (255 * color[2]) + ',' + (255 * color[3]) + ')';
-
-						
-						this.ctx.beginPath();
-						this.ctx.moveTo(ent.transform.position.x - 2.5, ent.transform.position.y - 2.5);
-						this.ctx.lineTo(ent.transform.position.x + 2.5, ent.transform.position.y + 2.5);
-						this.ctx.moveTo(ent.transform.position.x + 2.5, ent.transform.position.y - 2.5);
-						this.ctx.lineTo(ent.transform.position.x - 2.5, ent.transform.position.y + 2.5);
-
-						this.ctx.stroke();
-
-						this.ctx.strokeRect(ent.transform.position.x - ent.size.x/2 + 0.5,
-											ent.transform.position.y - ent.size.y/2 + 0.5,
-											ent.size.x - 1,
-											ent.size.y - 1);
-					}
-				}
-			}
-        }
+        // Keep the loop going
+        window.requestAnimationFrame( this.update.bind(this) );
     };
 
     PlixApp.prototype.createScene = function() {
@@ -141,10 +102,6 @@ define([
     PlixApp.prototype.attachScene = function(scene) {
     	this.scenes.push(scene);
     	scene.app = this;
-    };
-
-    PlixApp.prototype.end = function() {
-		this.state = 'lose';
     };
 
     return PlixApp;
--- src/plix/Scene.js
+++ src/plix/Scene.js
@@ -30,5 +30,44 @@ define([
 		return this;
 	};
 
+	Scene.prototype.update = function(tpf) {
+		if(this._physicsWorld) this._physicsWorld.update(tpf);
+			
+		this.entities.forEach(function(entity) {
+			if(typeof entity.script === 'function') entity.script(entity);
+			if(entity._components.physics) {
+				entity.transform.position.x = entity._components.physics.body.pos.x;
+				entity.transform.position.y = entity._components.physics.body.pos.y;
+			}
+		});
+	};
+
+	Scene.prototype.draw = function(ctx) {
+
+		ctx.strokeStyle = this.active ? 'magenta' : 'grey';
+
+		this.entities.forEach(function(ent) {
+
+			// Get color from entity
+			var color = ent.component('graphics').graphic.color;
+
+			ctx.strokeStyle = 'rgba(' + (255 * color[0]) + ',' + (255 * color[1]) + ',' + (255 * color[2]) + ',' + (255 * color[3]) + ')';
+
+			
+			ctx.beginPath();
+			ctx.moveTo(ent.transform.position.x - 2.5, ent.transform.position.y - 2.5);
+			ctx.lineTo(ent.transform.position.x + 2.5, ent.transform.position.y + 2.5);
+			ctx.moveTo(ent.transform.position.x + 2.5, ent.transform.position.y - 2.5);
+			ctx.lineTo(ent.transform.position.x - 2.5, ent.transform.position.y + 2.5);
+
+			ctx.stroke();
+
+			ctx.strokeRect(ent.transform.position.x - ent.size.x/2 + 0.5,
+								ent.transform.position.y - ent.size.y/2 + 0.5,
+								ent.size.x - 1,
+								ent.size.y - 1);
+		});
+	};
+
     return Scene;
 });
