c0ba5c602ca318acb84c04825cd7a1fba1765876
marcusstenbeck <marcus.stenbeck@gmail.com>
2015-04-18 10:16:16 -0700

    maybe correct paths 6

--- js/PlatformGameFactory.js
+++ js/PlatformGameFactory.js
@@ -1,13 +1,14 @@
 requirejs.config({
     //By default load any module IDs from ../../src
-    baseUrl: './src',
+    baseUrl: './',
     //except, if the module ID starts with "app",
     //load it from the ./examples directory. paths
     //config is relative to the baseUrl, and
     //never includes a ".js" extension since
     //the paths config could be for a directory.
     paths: {
-        lib: './lib'
+        lib: './lib',
+        plix: './src/plix'
     }
 });
 define([
