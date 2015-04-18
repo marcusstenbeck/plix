19145c1da4c7c6d947f0868be8ad639ee44d3dda
marcusstenbeck <marcus.stenbeck@gmail.com>
2015-04-18 10:15:04 -0700

    maybe correct paths 5

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-// requirejs.config({
-//     // Default loading base url
-//     baseUrl: './',
-//     //except, if the module ID starts with "app",
-//     //load it from the ./examples directory. paths
-//     //config is relative to the baseUrl, and
-//     //never includes a ".js" extension since
-//     //the paths config could be for a directory.
-//     paths: {
-//         lib: './lib',
-//         plix: './src/plix'
-//     }
-// });
+requirejs.config({
+    // Default loading base url
+    baseUrl: './',
+    //except, if the module ID starts with "app",
+    //load it from the ./examples directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        lib: './lib',
+        plix: './src/plix'
+    }
+});
 define([
     'plix/PlixApp',
     'js/PlatformGameFactory'
