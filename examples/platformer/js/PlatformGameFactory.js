49361240e382a0b65f67e28bd6e5a31052856345
marcusstenbeck <marcus.stenbeck@gmail.com>
2015-04-23 08:57:19 -0700

    feat: added more easing functions and moved them into a more "global" place

--- examples/platformer/js/PlatformGameFactory.js
+++ examples/platformer/js/PlatformGameFactory.js
@@ -39,6 +39,37 @@ define([
     var PHYSICS_LAYER_TWO = 0b10;
     var finished = false;
 
+    function easeOutElastic(duration, time) {
+        var t = time;
+        var b = 0;
+        var c = 1;
+        var d = duration;
+
+        var s=1.70158; var p=0; var a=c;
+        if (t===0) return b;  if ((t/=d)===1) return b+c;  if (!p) p=d*0.3;
+        if (a < Math.abs(c)) { a=c; s=p/4; }
+        else s = p/(2*Math.PI) * Math.asin (c/a);
+        return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+    }
+
+    function easeOutExpo(duration, time) {
+        var t = time;
+        var b = 0;
+        var c = 1;
+        var d = duration;
+
+        return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+    }
+
+    function easeOutCubic(duration, time) {
+        var t = time;
+        var b = 0;
+        var c = 1;
+        var d = duration;
+
+        return (t>=d) ? b+c : c*((t=t/d-1)*t*t + 1) + b;
+    }
+
     function finishLevel(scene) {
         if(!finished) {
             finished = true;
@@ -51,19 +82,6 @@ define([
             /**
              *  display finish graphic
              */
-
-            function theEase(duration, time) {
-                var t = time;
-                var b = 0;
-                var c = 1;
-                var d = duration;
-
-                var s=1.70158; var p=0; var a=c;
-                if (t===0) return b;  if ((t/=d)===1) return b+c;  if (!p) p=d*0.3;
-                if (a < Math.abs(c)) { a=c; s=p/4; }
-                else s = p/(2*Math.PI) * Math.asin (c/a);
-                return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
-            }
 
             var fg = PlatformGameFactory.createFinishEffect(scene);
 
@@ -79,7 +97,7 @@ define([
                 if(!ent.startTime) ent.startTime = ent.scene.app.timeElapsed;
 
                 var dt = ent.scene.app.timeElapsed - ent.startTime;
-                var f = theEase(1000, dt);
+                var f = easeOutElastic(1000, dt);
                 
                 ent.transform.position.x = ent.camEnt.transform.position.x;
                 ent.transform.position.y = ent.camEnt.transform.position.y;
