63f88e6d3cadf509f4911aea262da34f5ab5e384
marcusstenbeck <marcus.stenbeck@gmail.com>
2015-04-23 06:55:38 -0700

    feat: flip y-coordinates of loaded images in renderer

--- src/plix/WebGLRenderer.js
+++ src/plix/WebGLRenderer.js
@@ -236,7 +236,7 @@ define([
             var texture = gl.createTexture();
 
             gl.bindTexture(gl.TEXTURE_2D, texture);
-            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+            gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true); // gl coordinate system is fucked
             // Give the image to OpenGL
             // texImage2D(target, level, internalfmt, fmt, type, obj)
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, entity.components.graphics.graphic.image);
